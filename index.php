2d4ff0b37ac633587e9f9571e02b8d7f3ad9e68d
Nildo Vieira Pontes <nildo.pontes@bol.com.br>
2020-07-12 15:10:55 -0300

    Adicionar estrutura básica da página inicial

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+   $data = 'data.txt';
+   clearstatcache();
+   if(!file_exists($data)){
+      file_put_contents($data, '', FILE_APPEND);
+   }
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+      <title>Dominó Online - Tabuleiros</title>
+      <link type="text/css" rel="stylesheet" href="index.css">
+      <script src="index.js"></script> 
+   </head>
+   <body>
+	   <header></header>
+	   <div class="boards"></div>
+	   <footer></footer>
+   </body>
+</html>
