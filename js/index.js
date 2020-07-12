df75c8c38fbb26388b86478df231d058cf25c3b1
Nildo Vieira Pontes <nildo.pontes@bol.com.br>
2020-07-12 15:32:09 -0300

    Adicionar função setCookieSession() Essa função define no navegador do usuário o cookie responsável pela sessão do tabuleiro escolhido. Basicamente esta função é responsável por compartilhar a sessão entre os dois adversários.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-function setCookie(cname, cvalue, exdays){
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setCookieSession(ssid{
+  document.cookie = "PHPSESSID=" + ssid + ";path=/";
 } 
